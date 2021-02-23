53a3721a8d369c410020eb776b8fdc6368373895
yamachan0625 <napoli.080625@gmail.com>
2021-02-24 01:30:01 +0900

    open-wc

---
+++ my-todo/index.js
@@ -0,0 +1,3 @@
+export { TodoItem } from "./src/TodoItem.js";
+export { TodoInput } from "./src/TodoInput.js";
+export { MyTodo } from "./src/MyTodo.js";
---
+++ my-todo/my-todo.js
@@ -0,0 +1,7 @@
+import { TodoItem } from "./src/TodoItem.js";
+import { TodoInput } from "./src/TodoInput.js";
+import { MyTodo } from "./src/MyTodo.js";
+
+window.customElements.define("todo-item", TodoItem);
+window.customElements.define("todo-input", TodoInput);
+window.customElements.define("my-todo", MyTodo);
